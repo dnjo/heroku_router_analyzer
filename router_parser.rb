dd50cb296a71cb47d4dd59c1c4e93c8c9ca969b1
Dennis Johansson <dennis@dnjo.net>
2016-12-12 13:15:16 +0100

    Refactor RouterParser

--- router_parser.rb
+++ router_parser.rb
@@ -2,15 +2,20 @@ module RouterParser
   class << self
     def parse_events(events)
       events.each_with_object([]) do |event, parsed_events|
-        next unless valid_event? event
-        values = get_event_values event
-        ip = get_ip values
-        path = values[:path]
-        parsed_events << build_parsed_event(ip, path)
+        parsed_event = parse_single_event event
+        parsed_events << parsed_event if parsed_event
       end
     end
 
     private
+
+    def parse_single_event(event)
+      return nil unless valid_event? event
+      values = get_event_values event
+      ip = get_ip values
+      path = values[:path]
+      build_parsed_event ip, path
+    end
 
     def get_event_values(event)
       event_parts = event.split ' '
